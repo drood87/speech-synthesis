dcf3ea32ac38adc7190a1b6762cdef08afb6ce49
drood <dbernstein1987@hotmail.com>
2018-12-12 17:19:34 +1100

    populate voices

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+    voices = this.getVoices(); // populate voices array with pre-defined voices from the speech API
+    const voiceOptions = voices
+        .map(
+            voice =>
+                `<option value="${voice.name}">${voice.name} (${
+                    voice.lang
+                })</option>`
+        )
+        .join(""); // append all available voices in dropdown menu
+    voicesDropdown.innerHTML = voiceOptions;
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
